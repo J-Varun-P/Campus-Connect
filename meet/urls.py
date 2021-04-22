5642805dc0240659a1592203f2e20b26c206d1ba
Varun <varunpawarjetling@gmail.com>
2021-04-22 09:32:52 +0530

    first commit

---
+++ meet/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+
+
+urlpatterns = [
+    path("", views.login_view, name="login"),
+    path("register", views.register, name="register"),
+    path("logout", views.logout_view, name="logout"),
+    path("index", views.index, name="index"),
+]
---
+++ meet/views.py
@@ -0,0 +1,61 @@
+from django import forms
+from django.urls import reverse
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+
+def login_view(request):
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            messages.error(request, 'Invalid username and/or password.')
+            return HttpResponseRedirect(reverse("login"))
+    else:
+        return render(request, "meet/login.html")
+
+
+
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}, You can now Log In!')
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'meet/register.html', {'form': form})
+
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+
+
+def index(request):
+    return HttpResponseRedirect(f"TODO")
