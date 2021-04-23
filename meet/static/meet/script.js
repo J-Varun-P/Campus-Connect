6734f280e1371d2e8db3ba27e7f79885241c223f
Varun <varunpawarjetling@gmail.com>
2021-04-23 13:42:37 +0530

    Still JS

--- meet/static/meet/script.js
+++ meet/static/meet/script.js
@@ -2,5 +2,10 @@ document.addEventListener('DOMContentLoaded', () => {
   document.querySelector('#profile_form').style.display = 'none';
   document.querySelector('#updatebutton').onclick = () => {
     document.querySelector('#profile_form').style.display = 'block';
+    document.querySelector('#updatebutton').style.display = 'none';
+  };
+  document.querySelector('#closeform').onclick = () => {
+    document.querySelector('#profile_form').style.display = 'none';
+    document.querySelector('#updatebutton').style.display = 'block';
   };
 })
