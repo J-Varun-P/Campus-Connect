c33c73c5427a0de7322bf9d7922c5587ea90ede0
Varun Pawar Jetling <vj1012@wildcats.unh.edu>
2021-04-26 23:32:27 +0530

    fontawesome icons

--- meet/static/meet/script.js
+++ meet/static/meet/script.js
@@ -1,4 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
+
+  if(document.querySelector('#profile_form')){
   document.querySelector('#profile_form').style.display = 'none';
   document.querySelector('#updatebutton').onclick = () => {
     document.querySelector('#profile_form').style.display = 'block';
@@ -9,4 +11,21 @@ document.addEventListener('DOMContentLoaded', () => {
     document.querySelector('#profile_form').style.display = 'none';
     document.querySelector('#updatebutton').style.display = 'block';
   };
+}
+
+if(document.querySelector('.fontawesome_delete')){
+  document.querySelectorAll('.fontawesome_delete').forEach(item => {
+    item.onmouseover = () =>{
+      item.classList.remove('far');
+      item.classList.add('fas');
+    }
+  });
+  document.querySelectorAll('.fontawesome_delete').forEach(item => {
+    item.onmouseout = () =>{
+      item.classList.remove('fas');
+      item.classList.add('far');
+    }
+  });
+}
+
 })
